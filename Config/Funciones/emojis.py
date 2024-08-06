b74ee1ce7ffeff06e2b4cec9e0abe1b42b7c62d2
Jorge Alberto <47840499+Jorge4LV@users.noreply.github.com>
2024-08-06 01:19:45 -0600

    Actualizar emojis.py

--- Config/Funciones/emojis.py
+++ Config/Funciones/emojis.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,8 +18,12 @@ def get_emojis():
     
     if response.status_code == 200:
         emojis = response.json()
-        for emoji in emojis:
-            print(f"<:{emoji['name']}:{emoji['id']}>")
+        emojis_data = [{"name": emoji["name"], "id": emoji["id"]} for emoji in emojis]
+        
+        with open("emojis_list.json", "w") as file:
+            json.dump(emojis_data, file, indent=4)
+        
+        print("Emojis guardados en emojis_list.json")
     else:
         raise Exception(f"Failed to fetch emojis: {response.status_code}")
 
